768d4ca8e6c1a6c647aba6bbb55c19e91df093c2
haoyu-zhang <z2929473414@gmail.com>
2026-06-24 18:11:14 +0800

    Initial commit

---
+++ springboot-web-demo01/src/main/java/com/itheima/springbootwebdemo01/SpringbootWebDemo01Application.java
@@ -0,0 +1,13 @@
+package com.itheima.springbootwebdemo01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWebDemo01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootWebDemo01Application.class, args);
+    }
+
+}
---
+++ springboot-web-demo01/src/test/java/com/itheima/springbootwebdemo01/SpringbootWebDemo01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itheima.springbootwebdemo01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootWebDemo01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
